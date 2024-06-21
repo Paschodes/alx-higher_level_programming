b74b9059e97bb4626391736caf698fe38f3394b2
Paschodes <paschal.c.chikwendu@gmail.com>
2024-06-21 07:26:16 -0700

    a script that imports an array and computes a new array.

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 /* a script that imports an array and computes a new array. */
 
-const list = require('./100-map.js ').list;
+const list = require('./100-data').list;
 console.log(list);
 console.log(list.map((x, i) => x * i));
